f27b60df83e4a87f639d294e00a62340b62f9ab3
KARAN KUMAR <kkvmalhi@gmail.com>
2023-04-09 10:10:24 +0500

    basic stylink done

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
+let currentDate = new Date();
+let date = currentDate.getDate();
+let month = currentDate.getMonth() + 1;
+let year = currentDate.getFullYear();
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+
+// Format the date and time string
+const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUNE', 'JULY', 'AUG', 'SEPT', 'OCT', 'NOV', 'DEC'];
+
+let formattedDate = `${date < 10 ? '0' : ''}${date}-${months[month-1]}`;
+document.querySelector("#date").innerHTML = `<i class="bi bi-calendar3"></i> ${formattedDate}`;
+let ampm = hours >= 12 ? 'PM' : 'AM';
+let formattedTime = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
+document.querySelector("#time").innerHTML = `<i class="bi bi-clock-fill"></i> ${formattedTime}`;
+
+// js for dark mode
+// const toggleButton = document.querySelector("#dark-mode-toggle");
+// const body = document.querySelector("body");
+
+// toggleButton.addEventListener("click", () => {
+//   body.classList.toggle("dark-mode");
+// });
 window.getWeather = function () {
-  // Create a new date object
-  let currentDate = new Date();
-  // Get the date and time values
-  let date = currentDate.getDate();
-  let month = currentDate.getMonth() + 1;
-  let year = currentDate.getFullYear();
-  // let hours = currentDate.getHours();
-  // let minutes = currentDate.getMinutes();
-
-  // Format the date and time string
-  let formattedDate = `${year}-${month < 10 ? '0' : ''}${month}-${date < 10 ? '0' : ''}${date}`;
-  // let formattedTime = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-
-  // Display the date and time
-  document.querySelector("#date").innerHTML = `Date:${formattedDate}`
-  // document.querySelector("#time").innerHTML = `time:${formattedTime}`
-
-  // let counterName = document.querySelector("#counterName").value;
   let cityName = document.querySelector("#cityName").value;
   let API_KEY = "bd30503813924350feea847416d7bff9";
   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
     .then(function (response) {
       // handle success
       console.log(response.data);
-      document.querySelector("#temp").innerHTML = `Results for, `;
-      document.querySelector("#celsius").innerHTML = `${response.data.name} ${response.data.sys["country"]}<br>${response.data.main.temp}°C`;
-      document.querySelector("#frenhiet").innerHTML = `${(response.data.main.temp * 9 / 5) + 32}°F`;
-      document.querySelector("#otherInformation").innerHTML = `Humidity ${response.data.main.humidity}%<br>Feels Like ${response.data.main.feels_like}`
-      // document.querySelector("#otherInformation").innerHTML = `Humidity ${response.data.main.humidity}%<br>Feels Like ${response.data.main.feels_like} <br> ${response.data.main.weather.0["description"]}`
+      document.querySelector("#name").innerHTML = `Today's Weather of, ${response.data.name} ${response.data.sys["country"]}`;
+      document.querySelector("#celsius").innerHTML = `${response.data.main.temp}<span id="cel">°C</span>`;
+      document.querySelector("#frenhiet").innerHTML = `${(response.data.main.temp * 9 / 5) + 32}<span id="cel">°F</span>`;
+      document.querySelector("#humadity").innerHTML = `Humidity ${response.data.main.humidity}%`
+      document.querySelector("#feels_like").innerHTML = `Feels Like ${response.data.main.feels_like}<span id="cel">°C</span>`
+      document.querySelector("#status").innerHTML = ` ${response.data.weather[0].description}`
     })
     .catch(function (error) {
       // handle error
